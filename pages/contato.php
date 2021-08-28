434aa46541527ace07e3a81e1affc2751f6b37e3
Argemiro Zacouteguy <argemiro.zacouteguy@gmail.com>
2021-08-27 23:25:43 -0300

    first commit

---
+++ pages/contato.php
@@ -0,0 +1,20 @@
+	<div id="formulario">
+		<h1>Contato</h1>
+		Responderemos o mais breve possível!.
+		<form>
+			<label>Nome completo</label><br>
+			<input type="text" name=""><br/>
+			<label>E-mail</label><br/>
+			<input type="text" name=""><br/>
+			<label>Assunto</label><br/>
+			<input type="text" name=""><br/>
+			<label>Mensagem</label><br/>
+			<textarea>
+				
+			</textarea><br>
+			<button>Enviar</button>
+		</form>
+		<img src='images/doces.jpg'>
+	</div>
+	
+	
