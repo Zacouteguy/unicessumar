5020be7e8f54ae746322829256646a7407a9cf2b
Argemiro D Zacouteguy <45882588+Zacouteguy@users.noreply.github.com>
2021-08-27 23:34:05 -0300

    Add files via upload

---
+++ 20080286-5_Argemiro_Dutra_Zacouteguy/pages/quemsomo.php
@@ -0,0 +1,24 @@
+
+
+<div id="quemsomos">
+	
+ 
+ <p>
+ 	
+
+ What is Lorem Ipsum?
+Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.<br> It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem Ipsum is that it has a more-or-less normal distribution of letters, as opposed to using 'Content here, content here', making it look like readable English. Many desktop publishing packages and web page editors now use Lorem Ipsum as their default model text, and a search for 'lorem ipsum' will uncover many web sites still in their infancy. Various versions have evolved over the years, sometimes by accident, sometimes on purpose (injected humour and the like).<br>  
+	um estabelecimento alugado, com pouco mais de 80m2, um funcionário e quatro filhos, foram firmando seu espaço.<br><br>
+
+	It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem Ipsum is that it has a more-or-less normal distribution of letters, as opposed to using 'Content here, content here', making it look like readable English. Many desktop publishing packages and web page editors now use Lorem Ipsum as their default model text, and a search for 'lorem ipsum' will uncover many web sites still in their infancy. Various versions have evolved over the years, sometimes by accident, sometimes on purpose (injected humour and the like)..<br>
+Sempre preocupados em servir bem, traziam sempre novidades e pedidos dos clientes. Pouco a pouco a Malu foi crescendo, e o espaço inicial, <br> 
+tornou-se pequeno para tanta variedade. Precisaram de um novo espaço, muito maior que o primeiro, mudaram-se  então para o prédio proprio<br>
+  com 600m2, montando uma loja moderna, com auto-serviço e assim, criando sua própria identidade: um supermercado de Doces.<br><br>
+
+  It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem Ipsum is that it has a more-or-less normal distribution of letters, as opposed to using 'Content here, content here', making it look like readable English. Many desktop publishing packages and web page editors now use Lorem Ipsum as their default model text, and a search for 'lorem ipsum' will uncover many web sites still in their infancy. Various versions have evolved over the years, sometimes by accident, sometimes on purpose (injected humour and the like)..<br>
+
+físico da loja, hoje com 1200m2, seja no espaço cibernético com a inauguração da loja virtual.Então, se precisar de um docinho pra comer em casa, <br>
+It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem Ipsum is that it has a more-or-less normal distribution of letters, as opposed to using 'Content here, content here', making it look like readable English. Many desktop publishing packages and web page editors now use Lorem Ipsum as their default model text, and a search for 'lorem ipsum' will uncover many web sites still in their infancy. Various versions have evolved over the years, sometimes by accident, sometimes on purpose (injected humour and the like).<br>
+ de embalagens diversas, de artigos para a festa de seus filhos, com preço justo e confiança, visite nossas seções.
+ </p>
+</div>
